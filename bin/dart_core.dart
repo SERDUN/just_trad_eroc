b4039796cc5d46b5357dcb2c28830a23a227825c
Dmitro Serdun <dmitroserdun@Dmitros-MacBook-Pro.local>
2023-04-05 20:32:50 +0300

    feat: add import to run file

--- bin/dart_core.dart
+++ bin/dart_core.dart
@@ -1,1 +1,5 @@
+import 'package:dart_core/core/math/math.dart';
+import 'package:dart_core/core/list/list.dart';
+import 'package:dart_core/core/string/string.dart';
+
 void main(List<String> arguments) {}
