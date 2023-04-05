ece95c2144233b034c8d1ae22e38280d249e7ff3
Dmitro Serdun <dmitroserdun@Dmitros-MacBook-Pro.local>
2023-04-05 20:54:59 +0300

    feat: implement of task sum two numbers

--- bin/dart_core.dart
+++ bin/dart_core.dart
@@ -2,4 +2,7 @@ import 'package:dart_core/core/math/math.dart';
 import 'package:dart_core/core/list/list.dart';
 import 'package:dart_core/core/string/string.dart';
 
-void main(List<String> arguments) {}
+void main(List<String> arguments) {
+  final result = calculateSumTwoNumbers(number1: 5, number2: 5);
+  print(result);
+}
