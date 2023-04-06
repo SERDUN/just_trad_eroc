85c43ba0b8626776e7975a220d7c1b554364a4fe
Dmitro Serdun <dmitroserdun@Dmitros-MacBook-Pro.local>
2023-04-06 08:22:13 +0300

    feat: implement of inverting string

--- bin/dart_core.dart
+++ bin/dart_core.dart
@@ -3,6 +3,8 @@ import 'package:dart_core/core/list/list.dart';
 import 'package:dart_core/core/string/string.dart';
 
 void main(List<String> arguments) {
-  final result = calculateSumTwoNumbers(number1: 5, number2: 5);
+  // final result = calculateSumTwoNumbers(number1: 5, number2: 5);
+  // print(result);
+  final result = invertScreen(inputString: "String", type: Type.func);
   print(result);
 }
