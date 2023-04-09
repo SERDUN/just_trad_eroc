adde85be913794e94d6188156a21bd0af44989ff
Yura Kovalchuk <deersblabla@gmail.com>
2023-04-09 20:15:02 +0300

    refactor: use bool method

--- lib/core/string/string.dart
+++ lib/core/string/string.dart
@@ -1,2 +1,4 @@
 export 'invert_string.dart';
 export 'capital_letters.dart';
+export 'words_equality.dart';
+
---
+++ lib/core/string/words_equality.dart
@@ -0,0 +1,37 @@
+enum TypeWord {
+  array,
+  string,
+}
+
+bool wordsEquality({
+  required String inputString1,
+  required String inputString2,
+  TypeWord type = TypeWord.string,
+}) {
+  switch (type) {
+    case TypeWord.array:
+      return _checkingArray(inputString1, inputString2);
+    case TypeWord.string:
+      return _methodString(inputString1, inputString2);
+  }
+}
+
+bool _checkingArray(inputString1, inputString2) {
+  int str1 = inputString1.codeUnits.length;
+  int str2 = inputString2.codeUnits.length;
+  if (str1 == str2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+bool _methodString(String inputString1, String inputString2){
+  int str1 = inputString1.length;
+  int str2 = inputString2.length;
+  if (str1 == str2) {
+    return true;
+  } else {
+    return false;
+  }
+}
