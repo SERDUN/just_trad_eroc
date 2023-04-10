7f2ce18648f8a70ad16591c08fd7dbf7e37ed1c4
Yura Kovalchuk <deersblabla@gmail.com>
2023-04-10 13:55:51 +0300

    feat: fraction from division

---
+++ lib/core/math/fraction_from_division.dart
@@ -0,0 +1,6 @@
+fractionFromDivision({
+  required int number1,
+  required int number2,
+}) {
+  return number1 % number2;
+}
--- lib/core/math/math.dart
+++ lib/core/math/math.dart
@@ -3,3 +3,4 @@ export 'arithmetic_mean.dart';
 export 'prime_numbers.dart';
 export 'product_multiplication.dart';
 export 'even_number.dart';
+export 'fraction_from_division.dart';
