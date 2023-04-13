5d2cc93a1b9315a042cce29f69c27b5c342deeab
Yura Kovalchuk <deersblabla@gmail.com>
2023-04-13 16:04:55 +0300

    feat: add function to division by three or five

---
+++ lib/core/math/division_by_three_or_five.dart
@@ -0,0 +1,11 @@
+divisionByThreeOrFive({
+  required int number,
+}) {
+  List<int> dividers = [];
+  for (int i = 0; i <= number; i++) {
+    if (i % 3 & 5 == 0) {
+      dividers.add(i);
+    }
+  }
+  return dividers;
+}
