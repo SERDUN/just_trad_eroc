044f778e72c30453dea955e2165d0cfa3f03b5f8
Yura Kovalchuk <deersblabla@gmail.com>
2023-04-16 14:36:31 +0300

    style: change list posicion

--- bin/dart_core.dart
+++ bin/dart_core.dart
@@ -102,13 +102,13 @@ void main(List<String> arguments) {
   // );
   // print(result);
 
-  List<int> numbers = [65, 3, 10, 6, 4, 8, 11];
-
+  // List<int> numbers = [65, 3, 10, 6, 4, 8, 11];
   // final result = sumAllListElement(numbers:numbers,
   //   type: TypeMethod.forFunc,
   // );
   // print(result);
 
+  List<int> numbers = [65, 3, 10, 6, 4, 8, 11];
   final result = sumEvenListElements(numbers: numbers);
   print(result);
 }
