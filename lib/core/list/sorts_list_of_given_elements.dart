99c3078f6ceca657cb2007b74d2cd1f7b6e6242e
Yura Kovalchuk <deersblabla@gmail.com>
2023-04-20 15:03:10 +0300

    refactor: change place of the function break

--- lib/core/list/sorts_list_of_given_elements.dart
+++ lib/core/list/sorts_list_of_given_elements.dart
@@ -4,10 +4,10 @@ List<int> sortsListOfGivenElements({
 }) {
   int variable = 0;
   for (int i = 0; i < numbers.length; i++) {
+    if (i == givenElement) {
+      break;
+    }
     for (int j = 0; j < numbers.length; j++) {
-      if (i == givenElement) {
-        break;
-      }
       if (numbers[i] < numbers[j]) {
         variable = numbers[i];
         numbers[i] = numbers[j];
