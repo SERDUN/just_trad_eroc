875efd7f8d50853e920b8133bd74082bc7dbf5f3
Yura Kovalchuk <deersblabla@gmail.com>
2023-04-24 18:45:01 +0300

    feat: add function method that finds duplicates in the dart list

--- bin/dart_core.dart
+++ bin/dart_core.dart
@@ -211,8 +211,12 @@ void main(List<String> arguments) {
   //   secondName: "serdun",
   // );
   // print(result.getFullName());
-  
-  List<int> numbers = [-33, 3, 10, 6, -4, 8, 11, 10, 14];
-  final result = findMedianOfList(numbers: numbers);
-  print(result);
+
+  // List<int> numbers = [-33, 3, 10, 6, -4, 8, 11, 10, 14];
+  // final result = findMedianOfList(numbers: numbers);
+  // print(result);
+
+  List<int> numbers = [-33, 3, 10, 6, -4, 8, 11, 10, 10];
+  final result = reverificationUniqueValues(numbers: numbers);
+  print(result? "reverification" : "not reverification");
 }
