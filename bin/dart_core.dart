bb6aea7a82fd27fc8b0fe49ac5a01358e028e095
Yura Kovalchuk <deersblabla@gmail.com>
2023-05-04 18:00:40 +0300

    fix: merge branches

--- bin/dart_core.dart
+++ bin/dart_core.dart
@@ -300,15 +300,6 @@ void main(List<String> arguments) {
   //   matrix: numbers,
   // );
   // print(result);
-  // List<List<int>> numbers = [
-  //   [2, 2, 1],
-  //   [1, 6, 7],
-  //   [3, 3, 3],
-  // ];
-  // final result = findMinAndMaxValueInMatrix(
-  //   matrix: numbers,
-  // );
-  // print(result);
 
   final result = Car(40500, 'Mersedes', 1999, 2023);
   print(result.informationCar());
