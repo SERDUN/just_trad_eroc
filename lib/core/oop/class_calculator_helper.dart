189e6b17fc09a3cf9e9e710d36ea1d6fd64f94ab
Yura Kovalchuk <deersblabla@gmail.com>
2023-05-10 18:23:02 +0300

    feat: create class calculator helper

---
+++ lib/core/oop/class_calculator_helper.dart
@@ -0,0 +1,21 @@
+class CalculatorHelper {
+  static int multiplication(int number1, int number2) {
+    int result = number1 * number2;
+    return result;
+  }
+
+  static double division(int number1, int number2) {
+    double result = number1 / number2;
+    return result;
+  }
+
+  static int addition(int number1, int number2) {
+    int result = number1 + number2;
+    return result;
+  }
+
+  static int subtraction(int number1, int number2) {
+    int result = number1 - number2;
+    return result;
+  }
+}
