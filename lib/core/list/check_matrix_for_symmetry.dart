5f9d5e860ae1b043f6287d525529fd93391d2715
Yura Kovalchuk <deersblabla@gmail.com>
2023-05-15 13:09:23 +0300

    refactor: optimization code

--- lib/core/list/check_matrix_for_symmetry.dart
+++ lib/core/list/check_matrix_for_symmetry.dart
@@ -2,10 +2,10 @@ bool checkMatrixForSymmetry({
   required List<List<int>> matrix,
 }) {
   for (int i = 0; i < matrix.length; i++) {
+    if (matrix.length != matrix[i].length) {
+      return false;
+    }
     for (int j = i; j < matrix[i].length; j++) {
-      if (matrix.length != matrix[i].length) {
-        return false;
-      }
       if (matrix[i][j] != matrix[j][i]) {
         return false;
       }
